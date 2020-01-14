61259d07995eff0882c85a0856357f3094b8bf11
Alex <alexandre.barbillon@gmail.com>
2020-01-14 11:44:20 +0100

    Adding a Controller for games
    And the html layout for display

---
+++ src/main/java/ludopia/controller/GameController.java
@@ -0,0 +1,34 @@
+package ludopia.controller;
+
+import ludopia.objects.games.Game;
+import ludopia.objects.games.service.GameService;
+import ludopia.objects.users.LudopiaUser;
+import ludopia.objects.users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GameController {
+
+    @Autowired
+    GameService gameService;
+
+    @GetMapping("/addGame")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("gameCreation");
+        return mv;
+    }
+
+    @PostMapping("/addGame")
+    public ModelAndView registerNewTrainer(Game game) {
+        ModelAndView mv = new ModelAndView("gameCreation");
+        gameService.createGame(game);
+        System.out.println(gameService.getGamesSortByDate(3));
+        return mv;
+    }
+}
