781ff48900c25102cf1bccba047c1e46258dc1c1
Estelle HERIPRET <estelle.h62@hotmail.fr>
2020-01-14 15:06:35 +0100

    Auto stash before merge of "user_connection_registration" and "origin/develop"

--- src/main/java/ludopia/controller/UserController.java
+++ src/main/java/ludopia/controller/UserController.java
@@ -5,26 +5,24 @@ import ludopia.objects.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @GetMapping("/registration")
-    private String registration(Model model) {
+    public String registration(Model model) {
         model.addAttribute("userForm", new LudopiaUser());
         return "registration";
     }
 
     @PostMapping("/registration")
-    private String registration(@ModelAttribute("userForm") LudopiaUser user) {
+    public String submit(@ModelAttribute("userForm") LudopiaUser user) {
         userService.createUser(user);
-        return "redirect:/index";
+        return "index";
     }
 }
