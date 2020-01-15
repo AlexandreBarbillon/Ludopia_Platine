4228100d2f9e1a8c44df77ee5f8b852abf76bc27
Pierre Patte <pierre.patte21@orange.fr>
2020-01-15 13:39:47 +0100

    Ajout de la page d'un jeu, à tester

--- src/main/java/ludopia/controller/GameController.java
+++ src/main/java/ludopia/controller/GameController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,6 +30,13 @@ public class GameController {
         ModelAndView mv = new ModelAndView("gameCreation");
         gameService.createGame(game);
         System.out.println(gameService.getGamesSortByDate(3));
+        return mv;
+    }
+
+    @GetMapping("/game/{id}")
+    public ModelAndView displayGame(@PathVariable("id") int id) {
+        ModelAndView mv = new ModelAndView("jeu");
+        mv.addObject("game", gameService.getGameById(id));
         return mv;
     }
 }
