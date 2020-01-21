7be035b1649cf8a443c00d1fd5193910bab1d893
Estelle HERIPRET <estelle.h62@hotmail.fr>
2020-01-21 14:01:12 +0100

    New game page with stars rating

--- src/main/java/ludopia/config/SecurityConfig.java
+++ src/main/java/ludopia/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/registration","/addGame","/css/**", "/js/**", "/webfonts/**","/images/**").permitAll()
+                .antMatchers("/", "/registration","/addGame","/**/*.css","/js/**", "/webfonts/**","/images/**","/game/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
