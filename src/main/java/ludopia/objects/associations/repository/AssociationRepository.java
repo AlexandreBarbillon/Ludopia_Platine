087e66e7a6a9612e6932e372f06b8ae76ea7df78
Alex <alexandre.barbillon@gmail.com>
2020-01-29 11:08:41 +0100

    Correcting an other error appearing because a _ where removed

--- src/main/java/ludopia/objects/associations/repository/AssociationRepository.java
+++ src/main/java/ludopia/objects/associations/repository/AssociationRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface AssociationRepository extends CrudRepository<Association, String> {
     Optional<Association> findById(int id);
-    @Query("FROM Association as asso JOIN GameList as list ON asso.possessedGamesList = list.id WHERE :gameId MEMBER OF list.gameList")
+    @Query("FROM Association as asso JOIN GameList as list ON asso.possessedGamesList = list.id WHERE :gameId MEMBER OF list.games")
     List<Association> findAssoHavingTheGame(int gameId);
     List<Association> findAssociationsByAdmin(int userId);
 }
