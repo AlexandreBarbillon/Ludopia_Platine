87298b092dada0b20a675451ce3be3b5c9d51c24
Alex <alexandre.barbillon@gmail.com>
2020-02-03 18:17:14 +0100

    Adding a function to make average note for a game

--- src/main/java/ludopia/objects/games/service/GameService.java
+++ src/main/java/ludopia/objects/games/service/GameService.java
@@ -26,6 +26,19 @@ public interface GameService {
      */
     Game getGameById(int id);
 
+    /**
+     * Search a game by a substring
+     * @param search the substring of a game
+     * @return the list of game found with the key word
+     */
     List<Game> searchGame(String search);
+
+    /**
+     * go search a list by its id and transform all the games inside into Game
+     * @param listId the id of the list
+     * @return a list of Game
+     */
     List<Game> unwrapGameList(int listId);
+
+    int getAverageNoteFromGame(int gameId);
 }
